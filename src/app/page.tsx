046fed27eae3c5df91eefe29fc1dc5c18ec4c251
sideshow-charles <charlesonuoha97@gmail.com>
2025-09-04 09:14:53 +0100

    Project Base Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import '../app/assets/styles/pages/home.scss' 
 export default function Home() {
   return (
-    <div></div>
+    <div className="home__page">
+      <p className="hero__text">Welcome To Yomo Studios!</p>
+    </div>
   );
 }
